6569eaf0ceb42e97bb6eb524c9569ed2e9adf59f
Suzet Salinas <suzet.sal@gmail.com>
2024-02-27 17:06:52 -0600

    added files

---
+++ models/index.js
@@ -0,0 +1,35 @@
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
+
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
+
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'tags', // Alias when accessing Tags from Product
+  foreignKey: 'product_id',
+});
+
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'products', // Different alias when accessing Products from Tag
+  foreignKey: 'tag_id',
+});
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
